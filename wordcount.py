c6e398d9dcff2a11df1601fc232154c278b71051
Your Name <you@example.com>
2020-02-03 09:38:59 -0800

    Into Function

--- wordcount.py
+++ wordcount.py
@@ -1,16 +1,19 @@
 # put your code here.
-file_to_read = open("test.txt")
 
-all_words = []
-for line in file_to_read:
-    lines = line.strip()
-    words = lines.split(' ')
-    all_words.extend(words)
+def read_file(file):
+
+    file_to_read = open(file)
+
+    all_words = []
+    for line in file_to_read:
+        lines = line.strip()
+        words = lines.split(' ')
+        all_words.extend(words)
 
 
-word_count = {}
-for word in all_words:
-    word_count[word] = word_count.get(word, 0) + 1
+    word_count = {}
+    for word in all_words:
+        word_count[word] = word_count.get(word, 0) + 1
 
-for word, count in word_count.items():
-    print(f"{word} {count}") 
+    for word, count in word_count.items():
+        print(f"{word} {count}") 

e06808327d6d46ef5a23c9b065166dcd61d396b6
Your Name <you@example.com>
2020-02-04 09:53:38 -0800

    Working on Further Study

--- wordcount.py
+++ wordcount.py
@@ -4,23 +4,27 @@ def read_file(file):
 
     file_to_read = open(file)
 
+# read strip and split text file into a set of words.
+    
     all_words = []
     for line in file_to_read:
         lines = line.strip()
         words = lines.split(' ')
         all_words.extend(words)
 
-    print(all_words)
+
+# Remove all punctuations to the words
+    punctuations = [',', '.', '!', '?', ':', ';']
     for words in all_words:
         for letter in words:
-            if letter in [',', '.', '!', '?', ':', ';']:
-                print(words)
+            if letter in punctuations:
                 words = words[0:-1]
-                print(words)
+                all_words.append(words)
+    
 
     print(all_words)
 
-
+# Parse through list of words and add counter
     word_count = {}
     for word in all_words:
         word_count[word] = word_count.get(word, 0) + 1
